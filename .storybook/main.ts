e0c5f24bce3cd967b3c5617c0b694de38804df4e
Subin Lee <ewaterbinn@gmail.com>
2024-09-05 19:43:48 +0900

    deps: storybook 설치 및 기본 환경 세팅

---
+++ .storybook/main.ts
@@ -0,0 +1,34 @@
+import { resolve } from 'node:path';
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  typescript: {
+    reactDocgen: 'react-docgen',
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          '@': resolve(__dirname, '../src/'),
+        },
+      },
+    };
+  },
+};
+
+export default config;
